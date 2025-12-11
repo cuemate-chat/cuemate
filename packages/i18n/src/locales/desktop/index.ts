fc3c0ca382591b8aa2a291c6881c58abffca12f7
5566zzz03 <FLamberton5715@student.carlalbert.edu>
2025-12-11 20:30:21 +0800

    feat(i18n): add desktop locale files (zh-CN, zh-TW, en-US)

---
+++ packages/i18n/src/locales/desktop/index.ts
@@ -0,0 +1,11 @@
+import zhCN from './zh-CN.json' with { type: 'json' };
+import zhTW from './zh-TW.json' with { type: 'json' };
+import enUS from './en-US.json' with { type: 'json' };
+
+export { zhCN, zhTW, enUS };
+
+export default {
+  'zh-CN': zhCN,
+  'zh-TW': zhTW,
+  'en-US': enUS
+};
